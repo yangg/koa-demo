591724299a709e196911e709dc51f86664e3fa2a
Brook Yang <uedsky@gmail.com>
2017-04-09 21:44:11 +0800

    extract post model

--- index.js
+++ index.js
@@ -5,12 +5,11 @@ const router = require('koa-router')()
 const render = require('./lib/render')
 const bodyParser = require('koa-bodyparser')
 const session = require('koa-session')
+const model = require('./lib/post')
+// const model1 = require('./lib/post1')
 
 const app = new Koa()
 app.keys = [ 'secret key' ]
-
-const db = require('pg-promise')()({ host: 'localhost', database: 'koa_blog', user: 'brook' })
-const tableName = 'post'
 
 app.use(async (ctx, next) => {
   if (!/\.map$/.test(ctx.path)) {
@@ -59,37 +58,37 @@ app.use(async (ctx, next) => {
 })
 
 router.get('/', async function (ctx) {
-  const postList = await db.query('SELECT * FROM $1~ ORDER BY id DESC', [tableName])
+  const postList = await model.find()
   await ctx.render('index', { posts: postList })
 })
 
 router.get('/post/update/:id?', async function (ctx) {
-  const post = ctx.params.id ? await db.oneOrNone('SELECT * FROM $1~ WHERE id=$2', [tableName, ctx.params.id]) : {}
+  const post = ctx.params.id ? await model.findOne(ctx.params.id) : {}
   await ctx.render('update', { post })
 })
 router.post('/post/update/:id?', async function (ctx) {
-  const id = ctx.params.id
+  let id = ctx.params.id
   const post = ctx.request.body
   if (!id) {
     post.created_at = post.updated_at = new Date().getTime() / 1000
-    await db.query('INSERT INTO post(${this~}) VALUES(${title}, ${body}, ${created_at}, ${updated_at})', post)
+    id = await model.create(post)
+    console.log(id)
   } else {
     post.updated_at = new Date().getTime() / 1000
-    await db.query('UPDATE $1~ SET title=$2, body=$3, updated_at=$4 where id=$5',
-      [tableName, post.title, post.body, post.updated_at, id])
+    await model.update(id, post)
   }
   ctx.flash({type: 'success', msg: `Post ${id ? 'updated' : 'created'} successfully!`})
   ctx.redirect('/')
 })
 router.get('/post/:id?', async function (ctx) {
-  const post = await db.oneOrNone('SELECT * FROM $1~ WHERE id=$2', [tableName, ctx.params.id])
+  const post = await model.findOne(ctx.params.id)
   if (!post) {
     ctx.throw(404, 'Post not found')
   }
   await ctx.render('post', { post })
 })
 router.delete('/post/:id', async function (ctx) {
-  await db.query('DELETE FROM $1~ where id=$2', [tableName, ctx.params.id])
+  await model.delete(ctx.params.id)
   ctx.flash({type: 'success', msg: 'Post delete successfully!'})
   ctx.body = { code: 0 }
 })
