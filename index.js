bab1552ceb3afe2e48acaaaf7a799f315c4c8d1b
Brook Yang <uedsky@gmail.com>
2017-04-09 15:18:26 +0800

    migrate to postgresql

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const render = require('./lib/render')
 const bodyParser = require('koa-bodyparser')
 const session = require('koa-session')
 
-const db = require('monk')('localhost/koa_blog')
-
 const app = new Koa()
 app.keys = [ 'secret key' ]
-const posts = db.get('posts')
+
+const db = require('pg-promise')()({ host: 'localhost', database: 'koa_blog', user: 'brook' })
+const tableName = 'post'
 
 app.use(async (ctx, next) => {
   if (!/\.map$/.test(ctx.path)) {
@@ -38,7 +38,7 @@ app.use(async (ctx, next) => {
   try {
     await next()
     if (ctx.status === 404) {
-      ctx.throw('Page not found', 404)
+      ctx.throw(404, 'Page not found')
     }
   } catch (err) {
     let message = err.stack || err.message
@@ -59,43 +59,41 @@ app.use(async (ctx, next) => {
 })
 
 router.get('/', async function (ctx) {
-  const postList = await posts.find({}, {sort: { created_at: -1 }})
-  // console.log(ctx.session.flashMessages)
+  const postList = await db.query('SELECT * FROM $1~ ORDER BY id DESC', [tableName])
   await ctx.render('index', { posts: postList })
 })
 
 router.get('/post/update/:id?', async function (ctx) {
-  const post = ctx.params.id ? await posts.findOne({ _id: ctx.params.id }) : {}
+  const post = ctx.params.id ? await db.oneOrNone('SELECT * FROM $1~ WHERE id=$2', [tableName, ctx.params.id]) : {}
   await ctx.render('update', { post })
 })
 router.post('/post/update/:id?', async function (ctx) {
   const id = ctx.params.id
   const post = ctx.request.body
   if (!id) {
-    post.created_at = post.updated_at = new Date()
-    await posts.insert(post)
+    post.created_at = post.updated_at = new Date().getTime() / 1000
+    await db.query('INSERT INTO post(${this~}) VALUES(${title}, ${body}, ${created_at}, ${updated_at})', post)
   } else {
-    post.updated_at = new Date()
-    const postInfo = await posts.findOne({ _id: id })
-    Object.assign(postInfo, post)
-    await posts.update({ _id: id }, postInfo)
+    post.updated_at = new Date().getTime() / 1000
+    await db.query('UPDATE $1~ SET title=$2, body=$3, updated_at=$4 where id=$5',
+      [tableName, post.title, post.body, post.updated_at, id])
   }
   ctx.flash({type: 'success', msg: `Post ${id ? 'updated' : 'created'} successfully!`})
   ctx.redirect('/')
 })
 router.get('/post/:id?', async function (ctx) {
-  const post = await posts.findOne({ _id: ctx.params.id })
+  const post = await db.oneOrNone('SELECT * FROM $1~ WHERE id=$2', [tableName, ctx.params.id])
   if (!post) {
-    ctx.throw('Post not found', 404)
+    ctx.throw(404, 'Post not found')
   }
   await ctx.render('post', { post })
 })
-router.delete('/post/:id', function (ctx) {
-  posts.removeById(ctx.params.id)
+router.delete('/post/:id', async function (ctx) {
+  await db.query('DELETE FROM $1~ where id=$2', [tableName, ctx.params.id])
   ctx.flash({type: 'success', msg: 'Post delete successfully!'})
   ctx.body = { code: 0 }
 })
 
 app.use(router.routes())
 
-app.listen(3000)
+app.listen(process.env.PORT || 3000)
